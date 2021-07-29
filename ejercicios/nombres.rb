7aa4fdbcb3d06d8d105378302f24094a43e613c0
Evemurillo <84188077+Evemurillo@users.noreply.github.com>
2021-07-29 12:01:50 -0400

    nuevo

---
+++ ejercicios/nombres.rb
@@ -0,0 +1,18 @@
+Crear un arreglo de nombres y muestre lo siguiente
+a = {nombre: "Michael", apellido: "Choi"}
+b = {nombre: "John", apellido: "Doe"}
+c = {nombre: "Jane", apellido: "Doe"}
+d = {nombre: "James", apellido: "Smith"}
+e = {nombre: "Jennifer", apellido: "Smith"}
+
+nombres = [a, b, c, d, e]
+nombres.each do |elemento|
+    puts "El Nombre es:  #{elemento}".to_s 
+end
+
+
+
+
+
+
+
